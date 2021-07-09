96a794c447c944df35ba4768b74d46d62aa99bc0
Ali Raza <aliraza6136@gmail.com>
2021-07-09 13:03:29 -0700

    master

--- config/login.php
+++ config/login.php
@@ -3,6 +3,15 @@ include_once 'database.php';
 include_once 'session.php';
 Class Login
 {
+    private $db;
+    public function __construct(){
+            $this->db->database();
+    }
+
+    function createAccount($role,$name,$email,$pass)
+    {
+        $this->db->select("INSERT INTO user(name,email,password,role) VALUES('$role','$name','$email','$pass')");
+    }
 
 
 }
