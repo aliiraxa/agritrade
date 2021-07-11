7e52b2f3b6a6b8d1eccd746894fd1ff150d06d39
Ali Raza <aliraza6136@gmail.com>
2021-07-11 08:07:19 -0700

    master

--- agritrade.sql
+++ agritrade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2021 at 06:15 AM
+-- Generation Time: Jul 11, 2021 at 05:06 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -20,6 +20,55 @@ SET time_zone = "+00:00";
 --
 -- Database: `agritrade`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'rice'),
+(2, 'suger');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `price` decimal(9,2) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `phone` varchar(60) NOT NULL,
+  `category` int(11) NOT NULL,
+  `about` text NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `district` varchar(100) NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `img` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `user_id`, `title`, `price`, `name`, `email`, `phone`, `category`, `about`, `city`, `district`, `street`, `img`) VALUES
+(5, 4, 'FAUX LEATHER', '3434.00', 'Ali Razaa', 'aliraza6136@gmail.com', '0738345', 1, 'fasdfa', 'Gujrat Area', 'gsdfdf', '     32sdfsds3 Village AND Ladin gujrat tehsas djs alkd', 'assets/img/products/FAUXLEATHER.jpg'),
+(7, 4, 'For Writer', '0.00', 'Ali Raza', 'aliraza6136@gmail.com', '07383453599', 1, 'dsfad', 'Gujrat Area', 'fsdf', '32sdfsds3 Village AND Ladin gujrat tehsas djs alkd', 'assets/img/products/For Writer.PNG');
 
 -- --------------------------------------------------------
 
@@ -45,12 +94,24 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `title`, `name`, `img`, `address`, `about`, `phone`, `email`, `password`, `role`) VALUES
-(1, 'Mr', 'Ali Raza d', 'assets/img/1.', 'gurjat', 'ghfgh', '+9234178197193', 'siddm700@gmail.com', '12345', 1),
-(3, 'Mr', 'Ali Raza', 'assets/img/3.', 'gurjat', 'ghfgh', '+923417819719', 'aliraza6136@gmail.com', '123456', 2);
+(1, 'Mr', 'Ali Raza d', 'assets/img/1.PNG', 'gurjat', 'ghfgh', '+9234178197193', 'siddm7001@gmail.com', '12345', 1),
+(4, '', 'Ali Raza', '', 'gurjat', 'fadf', '07383453599', 'aliraza6136@gmail.com', '12345', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -63,10 +124,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
